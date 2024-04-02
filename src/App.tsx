dc4cc1f4ef192ca84e9badb0340ea22be08b8cb4
Naeem Ullah <106723598+NaeemUllah98@users.noreply.github.com>
2024-04-02 04:41:29 -0700

    frontend code

---
+++ src/App.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Loader from './common/Loader';
+import PageTitle from './components/PageTitle';
+import SignIn from './pages/Authentication/SignIn';
+import SignUp from './pages/Authentication/SignUp';
+import Calendar from './pages/Calendar';
+import Chart from './pages/Chart';
+import ECommerce from './pages/Dashboard/ECommerce';
+import Profile from './pages/Profile';
+import Tables from './pages/Tables';
+import Buttons from './pages/UiElements/Buttons';
+
+function App() {
+  const [loading, setLoading] = useState<boolean>(true);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Routes>
+        <Route
+          index
+          element={
+            <>
+              <PageTitle title="Dashboard" />
+              <ECommerce />
+            </>
+          }
+        />
+        <Route
+          path="/calendar"
+          element={
+            <>
+              <PageTitle title="Dashboard" />
+              <Calendar />
+            </>
+          }
+        />
+         <Route
+          path="/profile"
+          element={
+            <>
+              <PageTitle title="Profile" />
+              <Profile />
+            </>
+          }
+        /> 
+        
+        <Route
+          path="/tables"
+          element={
+            <>
+              <PageTitle title="Tables" />
+              <Tables />
+            </>
+          }
+        />
+        <Route
+          path="/chart"
+          element={
+            <>
+              <PageTitle title="Basic Chart" />
+              <Chart />
+            </>
+          }
+        />
+        
+        <Route
+          path="/ui/buttons"
+          element={
+            <>
+              <PageTitle title="Buttons" />
+              <Buttons />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signin"
+          element={
+            <>
+              <PageTitle title="Signin" />
+              <SignIn />
+            </>
+          }
+        />
+        <Route
+          path="/auth/signup"
+          element={
+            <>
+              <PageTitle title="Signup" />
+              <SignUp />
+            </>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Tables/TableOne.tsx
@@ -0,0 +1,94 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const TableOne = () => {
+  const [channel,getChannel] = useState<any>([])
+  const getChannels = async()=>{
+    try{
+      const response = await axios.get("http://localhost:5000/get-channel")
+      getChannel(response?.data?.result)
+    }catch(e){
+      console.log("e", e);
+      
+    }
+  }
+  useEffect(()=>{
+    getChannels();
+  }, [])
+  return (
+    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <h4 className="mb-6 text-xl font-semibold text-black dark:text-white">
+        Top Channels
+      </h4>
+
+      <div className="flex flex-col">
+        <div className="grid grid-cols-3 rounded-sm bg-gray-2 dark:bg-meta-4 sm:grid-cols-5">
+          <div className="p-2.5 xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Source
+            </h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Visitors
+            </h5>
+          </div>
+          <div className="p-2.5 text-center xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Revenues
+            </h5>
+          </div>
+          <div className="hidden p-2.5 text-center sm:block xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Sales
+            </h5>
+          </div>
+          <div className="hidden p-2.5 text-center sm:block xl:p-5">
+            <h5 className="text-sm font-medium uppercase xsm:text-base">
+              Conversion
+            </h5>
+          </div>
+        </div>
+
+        {channel?.map((brand: any, key: any) => (
+          <div
+            className={`grid grid-cols-3 sm:grid-cols-5 ${
+              key === channel.length - 1
+                ? ''
+                : 'border-b border-stroke dark:border-strokedark'
+            }`}
+            key={key}
+          >
+            <div className="flex items-center gap-3 p-2.5 xl:p-5">
+              <div className="flex-shrink-0">
+                <img src={brand?.image} alt="Brand" width={30} height={30}/>
+              </div>
+              <p className="hidden text-black dark:text-white sm:block">
+                {brand.source_name}
+              </p>
+            </div>
+
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-black dark:text-white">{brand.visitor_name}</p>
+            </div>
+
+            <div className="flex items-center justify-center p-2.5 xl:p-5">
+              <p className="text-meta-3">{brand.revenues}</p>
+            </div>
+
+            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+              <p className="text-black dark:text-white">{brand.sales}</p>
+            </div>
+
+            <div className="hidden items-center justify-center p-2.5 sm:flex xl:p-5">
+              <p className="text-meta-5">{brand.converstion}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TableOne;
